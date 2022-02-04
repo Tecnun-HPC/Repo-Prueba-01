131b3f6a7e66ffcbbac46f2c40b1859c9c64604a
Iker Mesa <ikermesa@gmail.com>
2022-02-04 12:15:41 +0100

    Creo que myVar2 debe ser 1111.

--- my_function_01.m
+++ my_function_01.m
@@ -8,7 +8,7 @@ function my_function_01(myInput01, myInput02)
     myVar = myInput01 + myInput02;
     disp(num2str(myVar));
 
-    myVar2 = 2342;
+    myVar2 = 1111; %En branch main pongo 1111.
     disp(num2str(myVar2));
 end
 
